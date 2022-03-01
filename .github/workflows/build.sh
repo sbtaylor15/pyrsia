08518f9de1c99e41f2e85eb2607b63daa0ac1e59
Steve Taylor <steve@deployhub.com>
2022-03-01 14:59:55 -0700

    print output

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -x
-nohup cargo build --workspace --verbose --release 2>/dev/null 1>/dev/null &
-nohup cargo build --tests --verbose --release 2>/dev/null 1>/dev/null &
+nohup cargo build --workspace --verbose --release 1>/tmp/ws.log 2>&1 &
+nohup cargo build --tests --verbose --release 1>/tmp/tests.log 2>&1 &
 jobs
 wait
+
+echo cargo build --workspace --verbose --release
+cat /tmp/ws.log
+
+echo cargo build --tests --verbose --release
+cat /tmp/tests.log
 
