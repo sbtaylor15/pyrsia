a967ca07cfe14a4004b721d5e0e907d945e8752b
Steve Taylor <steve@deployhub.com>
2022-03-01 14:55:40 -0700

    hide output

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
-nohup cargo build --workspace --verbose --release 2>&1 1>/dev/null &
-nohup cargo build --tests --verbose --release 2>&1 1>/dev/null &
+nohup cargo build --workspace --verbose --release 2>/dev/null 1>/dev/null &
+nohup cargo build --tests --verbose --release 2>/dev/null 1>/dev/null &
 jobs
 wait
 
